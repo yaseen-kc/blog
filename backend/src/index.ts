9e6c871f4d7edb6ee29e2edbf0a6f655ff105373
Yaseen KC <ymkcmpd@gmail.com>
2025-02-02 16:00:00 +0530

    feat: initialize route handlers for user authentication and blog management using Hono

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,14 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post(' /api/v1/user/signup', (c) => c.text('Signup Route - Post'))
+app.post(' /api/v1/user/signin', (c) => c.text('Signin Route - Post'))
+app.post(' /api/v1/blog', (c) => c.text('Blog - Post'))
+
+app.put('/api/v1/blog', (c) => c.text('Blog - Post'))
+
+app.get('/api/v1/blog:id', (c) => c.text('Blog - Get'))
+app.get('/api/v1/blog/bulk', (c) => c.text('Bulk - Post'))
+
 
 export default app

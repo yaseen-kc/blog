c45024ea1da0146b926d092903c6b80d62207a8e
Yaseen KC <ymkcmpd@gmail.com>
2025-02-03 16:00:00 +0530

    feat: add jwt sign authentication

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,15 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { sign } from 'hono/jwt'
 
 
 
 
 const app = new Hono<{
   Bindings: {
-    DATABASE_URL: string
+    DATABASE_URL: string,
+    JWT_SECRET: string
   }
 
 }>();
@@ -23,8 +25,8 @@ app.post('/api/v1/user/signup', async (c) => {
         password: body.password
       }
     })
-
-    return c.text('jwt here')
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET)
+    return c.json({ jwt })
   } catch (e) {
     return c.status(403)
   }

810427f2ed81f0f35d70aa447c5506468e3a9c52
Yaseen KC <ymkcmpd@gmail.com>
2025-02-03 16:00:00 +0530

    feat: add user signup route with Prisma and Accelerate

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,36 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
 
-app.post(' /api/v1/user/signup', (c) => c.text('Signup Route - Post'))
-app.post(' /api/v1/user/signin', (c) => c.text('Signin Route - Post'))
-app.post(' /api/v1/blog', (c) => c.text('Blog - Post'))
+
+
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string
+  }
+
+}>();
+app.post('/api/v1/user/signup', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate())
+  const body = await c.req.json()
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password
+      }
+    })
+
+    return c.text('jwt here')
+  } catch (e) {
+    return c.status(403)
+  }
+})
+app.post('/api/v1/user/signin', (c) => c.text('Signin Route - Post'))
+app.post('/api/v1/blog', (c) => c.text('Blog - Post'))
 
 app.put('/api/v1/blog', (c) => c.text('Blog - Post'))
 

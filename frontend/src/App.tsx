064a480755fb3a36db127a5eaf8afc1926b79250
Yaseen KC <ymkcmpd@gmail.com>
2025-02-08 18:06:00 +0530

    (frontend): Add React Router and implement routing for signup, signin, and blog pages; Add new Blog, Signin, Signup components.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Blog } from "./pages/Blog";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/blog/:id" element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
